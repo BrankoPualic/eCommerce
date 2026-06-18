f31762d8b970d750fa25caf630397b04e61fe5a9
Branko Pualić-Radujko <pualicbranko@gmail.com>
2026-06-18 22:57:10 +0200

    feat: users - add sqlite support with ef core + minimal endpoint structure

---
+++ eCommerce.Users.Api/Infrastructure/Persistence/DatabaseContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace eCommerce.Users.Api.Infrastructure.Persistence;
+
+public class DatabaseContext : DbContext
+{
+	public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options)
+	{ }
+}
--- eCommerce.Users.Api/Program.cs
+++ eCommerce.Users.Api/Program.cs
@@ -1,7 +1,13 @@
+using eCommerce.Users.Api.Endpoints;
+using eCommerce.Users.Api.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<DatabaseContext>(options => options.UseSqlite(builder.Configuration.GetConnectionString("Database")));
 
 var app = builder.Build();
 
-app.MapGet("/users", () => "Hello World!");
+app.MapUserEndpoints();
 
 app.Run();
